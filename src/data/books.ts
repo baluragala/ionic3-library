dce70131b5db4de691d104f4057b95b6a20f2fa5
Bala Krishna Ragala <baluragala@sparity.com>
2017-06-16 19:02:57 +0530

    Sample Data and Book Interface Added

---
+++ src/data/books.ts
@@ -0,0 +1,81 @@
+export default [
+  {
+    category: 'Science Fiction',
+    books: [
+      {
+        id: '1',
+        author: 'George Orwell',
+        title: 'Nineteen Eighty-Four'
+      },
+      {
+        id: '2',
+        author: 'Frank Herbert',
+        title: 'Dune'
+      },
+      {
+        id: '3',
+        author: 'Ray Bradbury',
+        title: 'Fahrenheit 451'
+      },
+      {
+        id: '4',
+        author: 'William Gibson',
+        title: 'Neuromancer'
+      },
+    ],
+    icon: 'brush'
+  },
+  {
+    category: 'Romance Novles ',
+    books: [
+      {
+        id: '5',
+        author: 'Jane Austen',
+        title: 'Pride and Prejudice'
+      },
+      {
+        id: '6',
+        author: 'Diana Gabaldon',
+        title: 'Outlander'
+      },
+      {
+        id: '7',
+        author: 'Erich Segal',
+        title: 'Love Story'
+      },
+      {
+        id: '8',
+        author: 'Charlotte Brontë',
+        title: 'Jane Eyre'
+      },
+    ],
+    icon: 'brush'
+  },
+  {
+    category: 'Inspirational',
+    books: [
+      {
+        id: '9',
+        author: 'Napoleon Hill',
+        title: 'Think and Grow Rich'
+      },
+      {
+        id: '10',
+        author: 'Norman Vincent Peale',
+        title: 'The Power of Positive Thinking'
+      },
+      {
+        id: '11',
+        author: 'Mitch Albom',
+        title: 'Tuesdays with Morrie'
+      },
+      {
+        id: '12',
+        author: 'Sun Tzu',
+        title: 'The complete art of war'
+      },
+    ],
+    icon: 'brush'
+  },
+
+];
